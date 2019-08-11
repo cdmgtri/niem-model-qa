cb91f5bef8c9674c63a04e503e982987e64b640e
CDM <>
2019-08-11 19:10:47 -0400

    Added Type test examples

--- src/type/unit/index.js
+++ src/type/unit/index.js
@@ -10,6 +10,11 @@ class TypeUnitTests extends ComponentUnitTests {
   /**
    * Check that complex types with simple content (CSC) have a CSC or a simple
    * base type.
+   *
+   * @example // Simple content type HairColorCodeType can have a base type like HairColorCodeSimpleType (simple type).
+   *
+   * @example // Simple content type HairColorCodeType cannot have a type like nc:PersonType (complex object type).
+   *
    * @param {Type[]} types
    * @param {Release} release
    */
@@ -33,6 +38,10 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that simple types have a XML schema base type.
+   *
+   * @example // Simple type ext:HairColorCodeSimpleType can have base type xs:token.
+   * @example // Simple type ext:HairColorCodeSimpleType should not have base type ext:ColorCodeSimpleType.
+   *
    * @param {Type[]} types
    * @param {Release} release
    */
@@ -56,6 +65,11 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that simple content types have a base.
+   *
+   * @example // Type HairColorCodeSimpleType needs a base type like xs:string or xs:token.
+   *
+   * @example // Type HairColorCodeSimpleType is not valid without a base type.
+   *
    * @param {Type[]} types
    */
   async base_missing_simpleContent(types) {
@@ -65,6 +79,10 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that type bases exist.
+   *
+   * @example // Type PersonType can extend type structures:ObjectType (this type exists).
+   * @example // Type PersonType cannot extend type structures:BogusType (this type does not exist).
+   *
    * @param {Type[]} types
    * @param {Release} release
    */
@@ -107,6 +125,11 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that a complex type definition begins with the opening phrase 'A data type '.
+   *
+   * @example // Definition 'A data type for a human being' is valid for type 'PersonType'.
+   *
+   * @example // Definition 'A human being' is not valid for type 'PersonType'.
+   *
    * @param {Type[]} types
    */
   async definition_phrase_complex(types) {
@@ -118,6 +141,11 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that a simple type definition begins with the opening phrase 'A data type '.
+   *
+   * @example // Definition 'A data type for United States state codes' is valid for type usps:StateCodeSimpleType.
+   *
+   * @example // Definition 'United States state codes' is not valid for type usps:StateCodeSimpleType.
+   *
    * @param {Type[]} types
    */
   async definition_phrase_simple(types) {
@@ -130,6 +158,11 @@ class TypeUnitTests extends ComponentUnitTests {
   /**
    * Check that words in a type definition are either in the dictionary or defined
    * as Local Terminology.
+   *
+   * @example // Definition 'A data type for FIPS state codes" is valid if the term 'FIPS' is defined as Local Terminology in that namespace.
+   *
+   * @example // Definition 'A data type for FIPS state codes" is not recommended if the term 'FIPS' is not defined as Local Terminology in that namespace.
+   *
    * @param {Type[]} types
    * @param {Release} release
    */
@@ -143,6 +176,10 @@ class TypeUnitTests extends ComponentUnitTests {
    * NDR exceptions:
    * - XML schema types (xs:string)
    * - Proxy XML schema types (niem-xs:string)
+   *
+   * @example // Type name 'PersonType' is valid because it begins with an upper case letter.
+   *
+   * @example // Type name 'personType' is not valid because it begins with a lower case letter.
 
    * @param {Type[]} types
    */
@@ -159,6 +196,10 @@ class TypeUnitTests extends ComponentUnitTests {
   /**
    * Check that type names are not repeated in a namespace.
    *
+   * @example // Types 'justice:CaseType' and 'logistics:CaseType' are valid because even though they use the same name, they are defined in different namespaces.
+   *
+   * @example // Type name 'CaseType' cannot be defined twice in the justice namespace.
+   *
    * @param {Type[]} types
    */
   async name_duplicate(types) {
@@ -168,6 +209,10 @@ class TypeUnitTests extends ComponentUnitTests {
   /**
    * Check that a type using representation term "CodeType" follows the same
    * naming pattern as its simple base type.
+   *
+   * @example // Type name 'HairColorCodeType' is valid with base type 'HairColorCodeSimpleType'.
+   *
+   * @example // Type name 'HairColorCodeType' is not recommended with base type 'EyeColorCodeSimpleType'.
    *
    * @param {Type[]} types
    */
@@ -182,6 +227,9 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that all type names use valid characters.
+   *
+   * @example // Type name "PersonType" uses valid characters.
+   * @example // Type name "ID#Type" does not use valid characters.
    *
    * @param {Type[]} types
    */
@@ -209,6 +257,11 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that a type with representation term "CodeSimpleType" declares codes.
+   *
+   * @example // Type name 'WeekdayCodeSimpleType' is valid if the type declares codes.
+   *
+   * @example // Type name 'WeekdayCodeSimpleType' is not valid if the type does not declare codes.
+   *
    * @param {Type[]} types
    */
   async name_repTerm_codeSimpleType(types) {
@@ -238,6 +291,10 @@ class TypeUnitTests extends ComponentUnitTests {
    * Check that a type using representation term "CodeType" has a base type
    * with representation term "CodeSimpleType".
    *
+   * @example // Type name 'WeekdayCodeSimpleType' is not valid if the type does not declare codes.
+   *
+   * @example // Type name 'WeekdayCodeType' is not valid with base type 'string'
+   *
    * @param {Type[]} types
    */
   async name_repTerm_codeType(types) {
@@ -251,6 +308,9 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that complex type names do not end with "SimpleType"
+   *
+   * @example // Type name 'IDSimpleType' is valid if the type is simple.
+   * @example // Type name 'IDSimpleType' is not valid if the type is complex.
    *
    * @param {Type[]} types
    */
@@ -267,6 +327,9 @@ class TypeUnitTests extends ComponentUnitTests {
    * Check that simple type names end with "SimpleType"
    *
    * NDR exceptions: XML schema types (xs:string)
+   *
+   * @example // Type name 'IDSimpleType' is valid if the type is simple.
+   * @example // Type name 'IDType' is not valid if the type is simple.
    *
    * @param {Type[]} types
    */
@@ -286,6 +349,9 @@ class TypeUnitTests extends ComponentUnitTests {
    * - XML schema types (xs:string)
    * - Proxy XML schema types (niem-xs:string)
    *
+   * @example // Type name 'PersonType' is valid.
+   * @example // Type name 'Person' is not valid.
+   *
    * @param {Type[]} types
    */
   async name_repTerm_type(types) {
@@ -300,6 +366,11 @@ class TypeUnitTests extends ComponentUnitTests {
   /**
    * Check that all type names do not use the term "Type" other than as the
    * final representation term.
+   *
+   * @example // Type name 'IDCategoryCodeType' is valid because the term 'Type' only appears at the end of the name.
+   *
+   * @example // Type name 'IDTypeCodeType' is not valid because the term 'Type' is used in the middle of the name.
+   *
    * @param {Type[]} types
    */
   async name_reservedTerm_type(types) {
@@ -310,6 +381,10 @@ class TypeUnitTests extends ComponentUnitTests {
   /**
    * Check that type name terms are either in the dictionary or defined in
    * Local Terminology.
+   *
+   * @example // Type name 'ncic:VMOCodeType' is valid if the ncic namespace defines the meaning of 'VMO' in its Local Terminology section.
+   *
+   * @example // Type name 'ncic:VMOCodeType' is not valid if the ncic namespace does not defined 'VMO' in its Local Terminology section.
    *
    * @param {Type[]} types
    * @param {Release} release
@@ -338,6 +413,10 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that types have a style.
+   *
+   * @example // Type PersonType has style 'object'.
+   * @example // Type PersonType does not have a style.
+   *
    * @param {Type[]} types
    */
   async style_missing(types) {
@@ -347,6 +426,10 @@ class TypeUnitTests extends ComponentUnitTests {
 
   /**
    * Check that types have a known style.
+   *
+   * @example // Type PersonType has permitted style 'object'.
+   * @example // Type PersonType cannot have unknown style 'myCustomObject'.
+   *
    * @param {Type[]} types
    */
   async style_unknown(types) {
